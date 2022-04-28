a50b7dbe371580c5cc0f68b07884bb0d0a4b30d3
Morswin <tomaszpowezk4@gmail.com>
2022-04-28 05:21:46 +0200

    Changed help command and more

--- bot.py
+++ bot.py
@@ -3,6 +3,11 @@ import discord
 from discord.ext import commands
 
 
+###########################
+#TODO   Section name here #
+###########################
+
+# Instantiating Bot and assigning it a command prefix
 client = commands.Bot(command_prefix='>')
 # TOKEN loading stuff
 # Forgive me for such defensive approach. 
@@ -24,6 +29,23 @@ except:
     raise ValueError("Failed to load the TOKEN.")
 
 
+#######################
+# Commands and Events #
+#######################
+
+# Removing and reassigning '>help' command
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+    print("Bot is online :D")
+
+# This command should be added as last one I think
+@client.command()
+async def help(ctx):
+    await ctx.channel.send("Test")
+
+
 def main():
     # For now it's enough that this file exists.
     pass
@@ -31,3 +53,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    client.run(TOKEN)  # Makes the Bot go online
