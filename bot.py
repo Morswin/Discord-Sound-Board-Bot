a4216845873528d8bf1cfbfec2510a98f7f2b1b8
Morswin <tomaszpowezk4@gmail.com>
2022-04-28 04:14:02 +0200

    Added the main file

---
+++ bot.py
@@ -0,0 +1,10 @@
+# It's ok that it's not used yet. It will be eventually.
+import discord.py
+
+
+def main():
+    # For now it's enough that this file exists.
+    pass
+
+if __name__ == "__main__":
+    main()
